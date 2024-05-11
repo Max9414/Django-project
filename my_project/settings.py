362d36a58c2a1bc7c1a577e311b055bad8ce03c2
Max94 <maxy_14@hotmail.it>
2024-05-11 21:33:03 +0000

    testing knowledge

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)_g7m2*y^c+z7u)c^%j$ce1xxtotz5&!u3)1yammndfvifeo-7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://my-django-project-hello-worldd-ef6291e76358','.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
 
 # Application definition
